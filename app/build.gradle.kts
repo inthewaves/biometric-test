f6d8e5617d68beb5606dc17df51501013f1b5dfc
inthewaves <inthewaves@pm.me>
2026-02-16 15:31:35 -0800

    decrement minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.biometrictest"
-        minSdk = 36
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
